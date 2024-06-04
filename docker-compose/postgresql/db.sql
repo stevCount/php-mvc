0cd933cabea1297832f0186e49f3a18e916dea29
Jhon S Vique <jhonsteven707@gmail.com>
2024-06-04 16:51:40 -0500

    Cambios creacion BD

--- docker-compose/postgresql/db.sql
+++ docker-compose/postgresql/db.sql
@@ -1,11 +1,11 @@
 GRANT CONNECT ON DATABASE php-mvc TO postgres;
 
-DROP TABLE IF EXISTS `informacion`;
-CREATE TABLE IF NOT EXISTS `informacion` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `texto` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `fecha` date DEFAULT NULL,
-  `estado` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `correo` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+DROP TABLE IF EXISTS informacion;
+CREATE TABLE IF NOT EXISTS informacion (
+  id SERIAL,
+  texto varchar(100) DEFAULT NULL,
+  fecha date default(NOW()),
+  estado varchar(100) DEFAULT NULL,
+  correo varchar(100) DEFAULT NULL,
+  PRIMARY KEY ("id")
+)
